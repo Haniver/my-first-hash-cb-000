4dab9aa3cee03ef1c3d7639ed33a33eeb04eeee0
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-06 20:47:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  the_hash = {"name" => "Lucio", "last name" => "Gutiérrez", "age" => "37"}
   
 end
 
